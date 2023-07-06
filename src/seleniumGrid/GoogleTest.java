7ca5e7190c983bb21f8b2286d05c79dd68850bf2
shellymutu-grigg <shellymutugrigg@gmail.com>
2023-07-06 13:43:01 +1200

    Section 27 Code update from teaching assistant - NOT WORKING

--- src/seleniumGrid/GoogleTest.java
+++ src/seleniumGrid/GoogleTest.java
@@ -6,7 +6,7 @@ import java.text.MessageFormat;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -15,13 +15,16 @@ public class GoogleTest {
 	@Test
 	public void HomePageCheck() throws MalformedURLException {
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setBrowserName("chrome");
+		// DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+		// capabilities.setBrowserName("chrome");
 		// capabilities.setAcceptInsecureCerts(true);
 		// capabilities.setCapability("acceptInsecureCerts", "true");
 		// capabilities.setCapability(CapabilityType.BROWSER_NAME, "chrome");
 
-		WebDriver webDriver = new RemoteWebDriver(new URL("http://192.168.1.19:4444"), capabilities);
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+
+		WebDriver webDriver = new RemoteWebDriver(new URL("http://192.168.1.19:4444"), options);
 		webDriver.get("https://www.google.com");
 
 		System.out.println(MessageFormat.format("Website title: {0}", webDriver.getTitle()));
--- src/seleniumGrid/SMGTest.java
+++ src/seleniumGrid/SMGTest.java
@@ -5,7 +5,7 @@ import java.net.URL;
 import java.text.MessageFormat;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -14,13 +14,15 @@ public class SMGTest {
 	@Test
 	public void HomePageCheck() throws MalformedURLException {
 
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setBrowserName("firefox");
+		// DesiredCapabilities capabilities = new DesiredCapabilities();
+		// capabilities.setBrowserName("firefox");
 		// capabilities.setAcceptInsecureCerts(true);
 		// capabilities.setCapability("acceptInsecureCerts", "true");
 		// capabilities.setCapability(CapabilityType.BROWSER_NAME, "firefox");
 
-		WebDriver webDriver = new RemoteWebDriver(new URL("http://192.168.1.19:4444"), capabilities);
+		ChromeOptions options = new ChromeOptions();
+
+		WebDriver webDriver = new RemoteWebDriver(new URL("http://192.168.1.19:4444"), options);
 		webDriver.get("https://www.shellymutugrigg.com");
 
 		System.out.println(MessageFormat.format("Website title: {0}", webDriver.getTitle()));
